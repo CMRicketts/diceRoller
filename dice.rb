61fd393817d592139eea8cffb95c22d398291dd6
CRicketts <31516691+CMRicketts@users.noreply.github.com>
2018-02-25 21:02:16 -0600

    Add files via upload

--- dice.rb
+++ dice.rb
@@ -1,25 +1,21 @@
 def roll(amount = 0, sides = 0)
-    srand
-    total = 0
-    (amount.to_i).times do 
-        total += rand(sides.to_i)
-    end
-    return total
+    amount.to_i.times.sum {|t| rand(1..sides.to_i)}
+end
+
+loop do
+    input = gets.chomp.to_s 
+    abort("May your rolls be ever natural") if input == "exit"
+    amount, sides, mod = input.tr("^0-9", " ").split
+    fin = roll(amount, sides) + mod.to_i
+    puts fin
 end
 
 
-while true
-    raw_input = gets.chomp.to_s
-    if raw_input == "exit"
-        abort("May your rolls be ever natural.")
-    end
-    proc_input = raw_input.tr("^0-9", " ")
-    output = proc_input.split()
-    if (output[2]) == nil 
-        mod = 0
-    else 
-        mod = output[2]
-    end
-    fin = roll(output[0], output[1]) + mod.to_i
-    puts "#{fin.to_s}"
-end
+##
+# inputs the ["1d4", "+", "4"]
+# want to input the 1d4 into the die, and roll functions
+# the die should just turn into a translator/processor for string -> dice roller
+# in main, we should check if the last two indicies a) exist and b) if '1' is + or -
+# and then add or subtract index 2 accordingly.
+# need to refactor die, roll, and main.
+# use 'a.pop(2)' to get the last two indices of the original array into a new array
